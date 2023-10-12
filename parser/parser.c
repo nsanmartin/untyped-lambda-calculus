226017585bb53a3833fd17f714f799b701dd022c
nico <smnicolas@gmail.com>
2023-10-12 19:56:54 -0300

    cli one file

--- parser/parser.c
+++ parser/parser.c
@@ -1,8 +1,22 @@
 #include <parser-lam-reader.h>
-
-int main (void) {
-    printf("> ");
-    parser_set_repl_fn();
-    yyin = stdin;
-    yyparse();
+#define YYDEBUG 1
+int main (int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("> ");
+        parser_set_repl_fn();
+        yyin = stdin;
+        yyparse();
+    } else {
+        for (int i = 1; i < argc; ++i) {
+	    FILE* file = fopen(argv[i], "r");
+	    if (file) {
+		    yyin = file;
+		    do {
+		        yyparse();
+		    } while (!feof(yyin));
+	    } else {
+		    fprintf(stderr, "error reading %s\n", argv[i]);
+	    }
+	}
+    }
 }
